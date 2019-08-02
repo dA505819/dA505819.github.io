772a017ad532a63d2f60264ebcfef039fa409b86
Dhruv <42606683+dA505819@users.noreply.github.com>
2019-08-03 00:53:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var i = 0;
-var txt = 'Dhruv Aggarwal'; /* The text */
+var txt = 'Hi, I am Dhruv Aggarwal'; /* The text */
 var speed = 50; /* The speed/duration of the effect in milliseconds */
 
 function typeWriter() {
