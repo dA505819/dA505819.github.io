ea04c5057fde0080fde176a0de7b8833f3e2f516
Dhruv <42606683+dA505819@users.noreply.github.com>
2019-08-02 23:35:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+var i = 0;
+var txt = 'DHRUV AGGARWAL'; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
 
